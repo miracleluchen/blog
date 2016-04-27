eb8e9310dfd2452ac147c214258e50e5be765987
luch <luch@garena.com>
2016-04-27 16:19:50 +0800

    update shell

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,8 @@
 mkdir -p /var/www/blog.lemontu.com
 
 cd ../web
-grunt build
+/home/luchen/node/bin/grunt build
 cp -rf dist/* /var/www/blog.lemontu.com/
 
-pm2 restart 0
+/home/luchen/node/bin/pm2 restart 0
 
